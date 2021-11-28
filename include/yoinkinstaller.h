b37fd24d5ec47ae89698ac87918c114ed638d84a
Gavin Pease <gavinpease@gmail.com>
2021-11-28 09:23:13 -0700

    Add yoinkinstaller to project
    Signed-off-by: Gavin Pease <gavinpease@gmail.com>

---
+++ include/yoinkinstaller.h
@@ -0,0 +1,14 @@
+//
+// Created by gcpease on 11/28/2021.
+//
+
+#ifndef YOINK_YOINKINSTALLER_H
+#define YOINK_YOINKINSTALLER_H
+
+
+class yoink_installer {
+
+};
+
+
+#endif //YOINK_YOINKINSTALLER_H
